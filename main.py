177349e913b79788dd0819eef5f47c6cdcd46b6f
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-04-05 14:02:44 +0900

    Adding foundational  architecture for the chatbot

---
+++ main.py
@@ -0,0 +1,30 @@
+import os 
+import json
+import random
+
+import nltk
+import numpy as np 
+import torch.nn as nn 
+import torch.nn.functional as F 
+import torch.optim as optim 
+from torch.utils.data import DataLoader, TensorDataset
+
+nltk.download("punkt_tab")
+
+class ChatbotModel(nn.Module):
+
+    def __init__(self, input_size, output_size):
+        super(ChatbotModel, self).__init__()
+
+        self.fc1 = nn.Linear(input_size, 120)
+        self.fc2 = nn.Linear(120, 64)
+        self.fc3 = nn.Linear(64, output_size)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
